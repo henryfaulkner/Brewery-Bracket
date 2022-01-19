37f5fa11acd9c63932c290e966b6c243ab556834
Henry Faulkner <hfaulkner27@gmail.com>
2022-01-18 19:31:56 -0500

    installed firebase; created firebase config file to initialize firebase and create firestore

---
+++ 404-brewery-bracket/pages/api/Firebase/FirebaseConfig.tsx
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import {getFirestore} from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+const app = initializeApp( {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID
+});
+
+const analytics = getAnalytics(app);
+const firestore = getFirestore();
+
+export {firestore};
