bc494564e69210fbf5971f8ba20c6ab89ab290a7
Drew Taylor <drew.taylor@perficient.com>
2022-02-08 18:31:41 -0500

    Comment out breaking page

--- 404-brewery-bracket/pages/bracket/index.tsx
+++ 404-brewery-bracket/pages/bracket/index.tsx
@@ -1,40 +1,40 @@
-import { GetBreweriesNamesAndIds } from "../api/BeerAPI/catalog-beer";
-import Card from "../../components/Card";
-import BracketCard from "../../components/Bracket";
-import Header from "../../components/Header";
+// import { GetBreweriesNamesAndIds } from "../api/BeerAPI/catalog-beer";
+// import Card from "../../components/Card";
+// import BracketCard from "../../components/Bracket";
+// import Header from "../../components/Header";
 
-const Bracket = ({ allBreweriesData }) => {
-  var breweryNames = [];
+// const Bracket = ({ allBreweriesData }) => {
+//   var breweryNames = [];
 
-  var breweryCards = allBreweriesData.map((breweryObj) => {
-    breweryNames.push(breweryObj.name);
-    return <Card name={breweryObj.name} />;
-  });
+//   var breweryCards = allBreweriesData.map((breweryObj) => {
+//     breweryNames.push(breweryObj.name);
+//     return <Card name={breweryObj.name} />;
+//   });
 
-  let brackets = [];
+//   let brackets = [];
 
-  while (breweryNames.length) {
-    let bracketCards = breweryNames.splice(0, 2);
-    let card1 = <Card name={bracketCards[0]} />;
-    let card2 = <Card name={bracketCards[1]} />;
-    // brackets.push(<BracketCard card1={card1} card2={card2} />);
-  }
+//   while (breweryNames.length) {
+//     let bracketCards = breweryNames.splice(0, 2);
+//     let card1 = <Card name={bracketCards[0]} />;
+//     let card2 = <Card name={bracketCards[1]} />;
+//     // brackets.push(<BracketCard card1={card1} card2={card2} />);
+//   }
 
-  return (
-    <div className="container">
-      <Header />
-      <div className="bracket">{brackets}</div>
-    </div>
-  );
-};
+//   return (
+//     <div className="container">
+//       <Header />
+//       <div className="bracket">{brackets}</div>
+//     </div>
+//   );
+// };
 
-export async function getStaticProps() {
-  // const allBreweriesData = await GetBreweriesNamesAndIds();
-  return {
-    // props: {
-    //   allBreweriesData,
-    // },
-  };
-}
+// export async function getStaticProps() {
+//   // const allBreweriesData = await GetBreweriesNamesAndIds();
+//   return {
+//     // props: {
+//     //   allBreweriesData,
+//     // },
+//   };
+// }
 
-export default Bracket;
+// export default Bracket;
