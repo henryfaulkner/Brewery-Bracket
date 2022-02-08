56915ac23fb0cc732d185547bae1157dd72833f6
Henry Faulkner <hlf46553@uga.edu>
2022-02-08 18:54:16 -0500

    remove class above header on bracket creator page

--- 404-brewery-bracket/pages/bracket-creator/index.tsx
+++ 404-brewery-bracket/pages/bracket-creator/index.tsx
@@ -1,42 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import globalStyles from '../../styles/Home.module.scss';
-import styles from '../../styles/BrackerCreator.module.scss';
+import globalStyles from "../../styles/Home.module.scss";
+import styles from "../../styles/BrackerCreator.module.scss";
 import Header from "../../components/Header";
 
 export default class BracketCreator extends React.Component {
-
-   
-
-    render() {
-        function addCustomBrewery() {
-
-        }
-        return (
-            <div className={globalStyles.container}>
-                <Header
-                />
-                <h2>Add Breweries</h2>
-                <hr />
-                <div className={styles.pageContentContainer}>
-                    <div className={styles.creationContainer}>
-                        <div className={styles.addBrewsCont}>
-                            <h3>Breweries</h3>
-                            <div className={styles.addCustomCont}>
-                                <div className={styles.labelInputPair}>
-                                    <label htmlFor="custom brewery">Custom Brewery</label>
-                                    <input id="custom brewery" type="text" autoComplete="name" />
-                                </div>
-                                <button onClick={() => addCustomBrewery()}>Add</button>
-                            </div>
-                        </div>
-                        <div className={styles.verticalDividerDiv}></div>
-                        <div className={styles.currentBreweries}>
-                            <h3>The Current Competition</h3>
-                        </div>
-                    </div>
+  render() {
+    function addCustomBrewery() {}
+    return (
+      <div>
+        <Header />
+        <h2>Add Breweries</h2>
+        <hr />
+        <div className={styles.pageContentContainer}>
+          <div className={styles.creationContainer}>
+            <div className={styles.addBrewsCont}>
+              <h3>Breweries</h3>
+              <div className={styles.addCustomCont}>
+                <div className={styles.labelInputPair}>
+                  <label htmlFor="custom brewery">Custom Brewery</label>
+                  <input id="custom brewery" type="text" autoComplete="name" />
                 </div>
+                <button onClick={addCustomBrewery()}>Add</button>
+              </div>
             </div>
-        )
-    };
+            <div className={styles.verticalDividerDiv}></div>
+            <div className={styles.currentBreweries}>
+              <h3>The Current Competition</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
