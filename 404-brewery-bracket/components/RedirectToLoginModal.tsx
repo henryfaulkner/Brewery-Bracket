a2b55ee86f0e2499134449140b335e39f6f6ffe0
Henry Faulkner <hlf46553@uga.edu>
2022-04-03 13:24:53 -0400

    Added RedirectToLoginModal, used when trying to create a bracket entry while not signed in.

---
+++ 404-brewery-bracket/components/RedirectToLoginModal.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import styles from "../styles/AdditionalInfoModal.module.scss";
+import Link from "next/link";
+
+type Props = {
+  showModal: {};
+  setShowModal;
+};
+
+const RedirectToLoginModal: React.FC<Props> = (props) => {
+  return (
+    <div className={styles.modal} style={props.showModal}>
+      <div className={styles.modalForm}>
+        <h2 className={styles.h2}>You'll need to login to do that.</h2>
+
+        <div className={styles.buttons}>
+          <button
+            className={styles.button1}
+            onClick={() => props.setShowModal({ display: "none" })}
+          >
+            Close
+          </button>
+          <Link href="/login-form">
+            <button
+              className={styles.button2}
+              onClick={() => {
+                props.setShowModal({ display: "none" });
+              }}
+            >
+              Go to Login
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RedirectToLoginModal;
