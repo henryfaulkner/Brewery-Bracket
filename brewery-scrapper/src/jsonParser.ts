7693d57243bf827ae9c0c026c4b5f26822c40a98
Henry Faulkner <hfaulkner27@gmail.com>
2022-05-26 22:56:07 -0400

    Reworked RemoveLinksFromJson

--- brewery-scrapper/src/jsonParser.ts
+++ brewery-scrapper/src/jsonParser.ts
@@ -2,11 +2,12 @@
 * Remove Name_links if the Name property 
 * came from an anchor tag.
 * @param JSON[] Json array from scrapped file
-* @returns Json array with Name as only property
+* @returns Json array without Name_link properties
 */
 function RemoveLinksFromJson(json) {
     let result = json.map(obj => {
-        return {Name: obj.Name}
+        delete obj["Name_link"]
+        return obj
     }) 
     
     return result
