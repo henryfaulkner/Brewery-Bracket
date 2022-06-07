771a8416c570ecd19d1d0fe078db5baae87d973e
Henry Faulkner <hfaulkner27@gmail.com>
2022-06-07 01:16:17 -0400

    Fixed the username functionality

--- 404-brewery-bracket/lib/hooks.ts
+++ 404-brewery-bracket/lib/hooks.ts
@@ -1,7 +1,7 @@
 import { auth, firestore } from './firebase';
 import { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { doc, collection } from 'firebase/firestore';
+import { doc, getDoc, getDocs, collection, query, where } from 'firebase/firestore';
 
 // Custom hook to read  auth record and user profile doc
 export function useUserData() {
@@ -11,17 +11,26 @@ export function useUserData() {
   useEffect(() => {
     // turn off realtime subscription
     let unsubscribe;
+    let authUsername;
+    
+    (async () => {
+      if (user) {
+        const userCollection = collection(firestore, "Users");
+        const q = query(userCollection, where("UserID", "==", auth.currentUser.uid));
+        await getDocs(q)
+          .then(userDocs => {
+            authUsername = userDocs.docs[0].data().Username;
+          });
 
-    if (user) {
-      const authUsername = collection(firestore, "Users", auth.currentUser.uid, "Username");
-      if(authUsername) {
-        setUsername(authUsername);
+        if(authUsername) {
+          setUsername(authUsername);
+        } else {
+          setUsername(null);
+        }
       } else {
         setUsername(null);
       }
-    } else {
-      setUsername(null);
-    }
+    })();
 
     return unsubscribe;
   }, [user]);
--- 404-brewery-bracket/pages/account/index.tsx
+++ 404-brewery-bracket/pages/account/index.tsx
@@ -13,10 +13,12 @@ const Account = () => {
       <a className={styles.loginLink}>Go to login</a>
     </Link>
   );
+  
   return (
     <div>
       <div>
         <h1>Account Information</h1>
+        <h3>Username: {username}</h3>
         <h3>Email: {user?.email ?? "not logged in"}</h3>
         <h3>Visted Breweries</h3>
         <hr />
