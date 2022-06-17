182c8ca0d9696862ae71a105c4411e344c486efa
Henry Faulkner <hfaulkner27@gmail.com>
2022-06-16 20:21:13 -0400

    Added endpoints for GetBeerByName and GetBreweryByName

---
+++ 404-brewery-bracket/pages/api/Firebase/Endpoints/GetBeerByName.ts
@@ -0,0 +1,72 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {
+  collection,
+  query,
+  getDocs,
+  where,
+  QuerySnapshot,
+  QueryDocumentSnapshot,
+  DocumentData,
+} from "firebase/firestore";
+
+import * as collectionConstants from "../CollectionConstants";
+import { firestore } from "../../../../lib/firebase";
+import Cors from 'cors';
+import { runMiddleware } from "../../middleware";
+import CustomBeer from "../Models/CustomBeer";
+
+const cors = Cors({
+  methods: ['POST', 'HEAD'],
+});
+
+/** 
+ * @swagger
+ *  /api/Firebase/Endpoints/GetBeerByName:
+ *    post:
+ *      summary: Get Beer by Name
+ *      description: Get a Beer object by name.
+ *      requestBody:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              beerName: string
+ *            example:
+ *              beerName: Coors Light
+ *      responses:
+ *        '200':
+ *          description: OK
+ *          content: 
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/CustomBeer'
+*/
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<CustomBeer>
+) => {
+  await runMiddleware(req, res, cors);
+  const beerName = req.body["beerName"];
+
+  const collectionRef = collection(firestore, collectionConstants.CustomBeers);
+  const q1 = await query(
+    collectionRef,
+    where("Name", "==", beerName)
+  );
+
+  const beerDocs: QuerySnapshot<DocumentData> = await await getDocs(q1);
+  const beerDoc: QueryDocumentSnapshot<DocumentData> = beerDocs.docs[0];
+  const response: CustomBeer = new CustomBeer({
+      DocumentID: beerDoc.id,
+      Name: beerDoc.data().Name, 
+      Description: beerDoc.data().Description, 
+      AssociatedBreweryID: beerDoc.data().AssociatedBreweryID, 
+      AssociatedBreweryName: beerDoc.data().AssociatedBreweryName, 
+      ABV: beerDoc.data().ABV, 
+      IBU: beerDoc.data().IBU, 
+      Style: beerDoc.data().Style, 
+  });
+
+  res.status(200).json(response);
+}
+
+export default handler;
---
+++ 404-brewery-bracket/pages/api/Firebase/Endpoints/GetBreweryByName.ts
@@ -0,0 +1,73 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {
+  collection,
+  query,
+  getDocs,
+  where,
+  QuerySnapshot,
+  QueryDocumentSnapshot,
+  DocumentData,
+} from "firebase/firestore";
+
+import * as collectionConstants from "../CollectionConstants";
+import { firestore } from "../../../../lib/firebase";
+import Cors from 'cors';
+import { runMiddleware } from "../../middleware";
+import BreweryObject from "../Models/BreweryObject";
+
+const cors = Cors({
+  methods: ['POST', 'HEAD'],
+});
+
+/** 
+ * @swagger
+ *  /api/Firebase/Endpoints/GetBreweryByName:
+ *    post:
+ *      summary: Get Brewery by Name
+ *      description: Get a Brewery object by name.
+ *      requestBody:
+ *        content:
+ *          application/json:
+ *            schema:
+ *              breweryName: string
+ *            example:
+ *              breweryName: hWWNwskdGOnEdq0KIQ3S
+ *      responses:
+ *        '200':
+ *          description: OK
+ *          content: 
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/BreweryObject'
+*/
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<BreweryObject>
+) => {
+  await runMiddleware(req, res, cors);
+  const breweryName = req.body["breweryName"];
+
+  const collectionRef = collection(firestore, collectionConstants.CustomBreweries);
+  const q1 = await query(
+    collectionRef,
+    where("Name", "==", breweryName)
+  );
+
+  const breweryDocs: QuerySnapshot<DocumentData> = await await getDocs(q1);
+  const breweryDoc: QueryDocumentSnapshot<DocumentData> = breweryDocs.docs[0];
+  const response: BreweryObject = new BreweryObject({
+      DocumentID: breweryDoc.id,
+      Name: breweryDoc.data().Name, 
+      Description: breweryDoc.data().Description, 
+      Short_Description: breweryDoc.data().Short_Description, 
+      Url: breweryDoc.data().Url, 
+      Facebook_Url: breweryDoc.data().Facebook_Url, 
+      Twitter_Url: breweryDoc.data().Twitter_Url, 
+      Instagram_Url: breweryDoc.data().Instagram_Url, 
+      Address: breweryDoc.data().Address, 
+  });
+
+  res.status(200).json(response);
+}
+
+export default handler;
